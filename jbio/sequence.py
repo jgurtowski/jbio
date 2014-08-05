5c3dfa4eb34c265baf6e7154a45d8da37f9175eb
James Gurtowski <gurtowsk@cshl.edu>
2014-08-05 16:19:12 -0400

    Fixed bug with fasta reader

--- jbio/sequence.py
+++ jbio/sequence.py
@@ -3,27 +3,27 @@ from collections import namedtuple
 FastaRecord = namedtuple('FastaRecord', ['name','seq'])
 FastqRecord = namedtuple('FastqRecord', ['name','seq','desc','qual'])
 
-
-#BUG: Seems to be broken when given an iterator_over_file
-# that only has one entry
 def fasta_iterator(iterable):
     '''Iterable is just a collection of strings
     Could be Lines
     Expects that ">" will be at the start of entries
     '''
     it = iter(iterable)
-    first = it.next()
-    
+    try:
+        first = it.next()
+    except StopIteration:
+        first = None
+
     if not first or not first.startswith(">"):
         raise Exception("No \'>\' at start of Fasta")
     name = first.strip()[1:]
     seq = ""
     while True:
-        #print "tippy"
-        l = it.next()
-        #print "top"
+        try:
+            l = it.next()
+        except StopIteration:
+            l = None
         if not l or l.startswith(">"):
-            #print "in if"
             yield FastaRecord(name, seq)
             if not l:
                 break
@@ -31,4 +31,3 @@ def fasta_iterator(iterable):
             seq = ""
         else:
             seq += l.strip()
-            #print "adding seq"
